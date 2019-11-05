e66b119279ab50c310b3a638342b860b83d0eef8
Alexander Linke <linke@gesundheitsforen.net>
2019-11-05 17:23:03 +0100

    Small Role Model Class fix

--- src/main/java/net/gesundheitsforen/sensordb/model/Role.java
+++ src/main/java/net/gesundheitsforen/sensordb/model/Role.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 public class Role {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
